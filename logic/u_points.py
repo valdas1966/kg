63c7163b90fd9911ba4f73b7c1625cdafec8c220
faiterbe <43270828+faiterbe@users.noreply.github.com>
2021-01-24 21:55:23 +0200

    kg

--- logic/u_points.py
+++ logic/u_points.py
@@ -1,5 +1,6 @@
 import random
 from collections import defaultdict
+from algo.astar import AStar
 
 
 def random_pairs_by_distance(grid, amount, size=1):
@@ -26,3 +27,23 @@ def random_pairs_by_distance(grid, amount, size=1):
             distance = (distance // size) * size
             pairs[distance].append((point_a, point_b))
     return pairs
+
+
+def is_clean_line(grid, point_a, point_b):
+    """
+    ============================================================================
+     Description: Return True if there is a Clean Line between the Points
+                    (no obstacles on the airline)
+    ============================================================================
+     Arguments:
+    ----------------------------------------------------------------------------
+        1. grid : GridBlocks
+        2. point_a : Point
+        3. point_b : Point
+    ============================================================================
+     Return: bool
+    ============================================================================
+    """
+    astar = AStar(grid, point_a, point_b)
+    astar.run()
+    return len(astar.optimal_path()) == point_a.distance(point_b)
