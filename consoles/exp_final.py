d16f4b111b2e1245d1ad6e16ebe002e10192a0aa
valdasivanauskas <valdas.ivanauskas.1966@gmail.com>
2021-03-15 15:25:55 +0200

    final

--- consoles/exp_final.py
+++ consoles/exp_final.py
@@ -2,11 +2,14 @@ from collections import defaultdict
 from logic import u_grid_blocks
 from f_utils import u_file
 from f_utils import u_pickle
+import pandas as pd
 
 
 dir_maps = 'd:\\temp\\maps\\'
 dir_storage = 'd:\\temp\\final\\'
 pickle_grids = dir_storage + 'grids.pickle'
+pickle_grids_final = dir_storage + 'grids_final.pickle'
+pickle_sg_pairs = dir_storage + 'sg_pairs.pickle'
 csv_grids = dir_storage + 'grids.csv'
 
 
@@ -35,5 +38,37 @@ def print_grids():
     file.close()
 
 
+def create_grids_final():
+    grids = u_pickle.load(pickle_grids)
+    grids_final = dict()
+    df = pd.read_csv(csv_grids)
+    df = df[df['is_final'] == 1]
+    for row in df.iterrows():
+        domain = row[1]['domain']
+        map = row[1]['map']
+        if domain not in grids_final:
+            grids_final[domain] = dict()
+        grids_final[domain][map] = grids[domain][map]
+    u_pickle.dump(grids_final, pickle_grids_final)
+
+
+def create_sg_pairs():
+    d_sg_pairs = dict()
+    d_grids = u_pickle.load(pickle_grids_final)
+    for domain in d_grids:
+        d_sg_pairs[domain] = dict()
+        for map, grid in d_grids[domain].items():
+            pairs = u_grid_blocks.random_pairs_by_distance(grid,
+                                                           amount=10000000,
+                                                           size=100)
+            d_sg_pairs[domain][map] = pairs
+            print(domain, map)
+    u_pickle.dump(d_sg_pairs, pickle_sg_pairs)
+
+
+
 # create_grids()
-print_grids()
+# print_grids()
+# create_grids_final()
+create_sg_pairs()
+
